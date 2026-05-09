7e171ea1907a432f8cbef220dd7b1e3461c7a552
maxfriis <45585452+maxfriis@users.noreply.github.com>
2026-05-09 11:30:10 +0200

    Trimmed code.

--- early-init.el
+++ early-init.el
@@ -1,25 +1,19 @@
 ;;; early-init.el --- -*- lexical-binding: t; -*-
 
 ;; ============================================================================
-;;; Early variables
+;;; Variables
 ;; ============================================================================
-;; Modern computers are able to handle more than the defaults.
-;; Garbage collection start at 50MB rather than 800k etc.
 (setq
+ ;; The recommended max read chunk for my system.
+ read-process-output-max (* 1024 1024)
+ ;; Garbage collection start at 50MB rather than 800k etc.
  gc-cons-threshold (* 50 1024 1024)
  gc-cons-percentage .5
- ;; The recommended max read chunk for my system:
- read-process-output-max (* 1024 1024)
  ;; Recalculating frame size is expencive if fonts/modeline change.
  frame-inhibit-implied-resize t
  ;; Trigger fewer garbage collections but use more memory.
- inhibit-compacting-font-caches t)
-
-;; ============================================================================
-;;; Vanilla defaults
-;; ============================================================================
-;; Clean up defaults I don't want to see even blink during startup.
-(setq
+ inhibit-compacting-font-caches t
+ ;; Clean defaults I don't want.
  inhibit-startup-message t
  mode-line-format nil
  frame-title-format "GNU Emacs"
@@ -28,9 +22,13 @@
    (foreground-color . "#fff")
    (background-color . "#000")
    (cursor-color     . "#0f0")))
+
+;; ============================================================================
+;;; Default modes
+;; ============================================================================
 (blink-cursor-mode -1)
-(scroll-bar-mode   -1)
 (menu-bar-mode     -1)
+(scroll-bar-mode   -1)
 (tool-bar-mode     -1)
 (tooltip-mode      -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
